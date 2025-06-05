c58700fa7e7a40eb655b192aba03cc520925e0eb
Dan Stoianovici <stoianovici.dan.s@gmail.com>
2025-06-05 00:30:33 -0400

    matplot lib working in demo

---
+++ workspace/src/examples/REINFORCE_example/cart_pole.py
@@ -0,0 +1,4 @@
+#! /usr/bin/python
+
+# Example from https://gymnasium.farama.org/tutorials/training_agents/reinforce_invpend_gym_v26/#sphx-glr-tutorials-training-agents-reinforce-invpend-gym-v26-py
+
